e9728b8b0805831212f4cf2450073605d64acc3f
Xolo <solomonmanyaga@gmail.com>
2020-04-29 11:43:49 -0400

    code cleanup

--- src/scripts/grid.js
+++ src/scripts/grid.js
@@ -4,19 +4,6 @@ class Grid{
     }
     
     render(){
-        // this.context.fillText(8, 200, 50);   
-        // this.context.fillText('22', 200, 140);
-
-        // this.context.fillText('22', 40, 210);
-        // this.context.fillText('22', 350, 400);
-
-        // for (let i = i; i < 10; i++) {
-        //     // setTimeout
-        //     this.context.fillText('hi', 20*i, 50);     
-        //     debugger       
-        // }
-
-
         for (let i = 0; i < 9; i++) {
             this.context.moveTo(i * 150, 0);
             this.context.lineTo(i * 150, 450);
@@ -31,12 +18,13 @@ class Grid{
         }
 
         for (let i = 0; i < 9; i++) {
-            this.context.lineWidth = 1;
+            this.context.lineWidth = 0.5;
             this.context.moveTo(i * 50, 0);
             this.context.lineTo(i * 50, 450);
             this.context.stroke();
         }
         for (let j = 0; j < 9; j++) {
+            this.context.lineWidth = 0.5;
             this.context.moveTo(0, j * 50);
             this.context.lineTo(450, j * 50);
             this.context.stroke();
